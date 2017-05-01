9a210329e1239a31047458540b4444fb9af7cde6
kiricon <kiricon@live.com>
2017-04-30 21:22:24 -0600

    Added comments.

--- rapid.go
+++ rapid.go
@@ -2,6 +2,7 @@ package rapid
 
 import "net/http"
 
+// Connection - struct for handling http request and write
 type Connection struct {
 	R *http.Request
 	W http.ResponseWriter
@@ -9,12 +10,14 @@ type Connection struct {
 
 type routeHandler func(Connection)
 
+// Route - Create a route for your webserver
 func Route(path string, handler routeHandler) {
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		handler(Connection{r, w})
 	})
 }
 
+// Start - Start webserver on specified port
 func Start(port string) {
 	http.ListenAndServe(":"+port, nil)
 }
