013c85ad5ab29c7441bf20d1ec08492932aa1b19
kiricon <kiricon@live.com>
2017-05-01 21:14:36 -0600

    Very close with url parameters.

--- rapid.go
+++ rapid.go
@@ -5,20 +5,24 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 // Connection - struct for handling http request and write
 type Connection struct {
-	R *http.Request
-	W http.ResponseWriter
+	R      *http.Request
+	W      http.ResponseWriter
+	Params map[string]string
 }
 
 type routeHandler func(Connection) string
 
 // Route - Create a route for your webserver
 func Route(path string, handler routeHandler) {
+	params, path := getParams(path)
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		resp := handler(Connection{r, w})
+
+		resp := handler(Connection{r, w, params})
 
 		if resp != "" {
 			fmt.Fprintf(w, resp)
@@ -48,4 +52,21 @@ func PublicFolder(path string) {
 func StartServer(port int) {
 	portString := strconv.Itoa(port)
 	http.ListenAndServe(":"+portString, nil)
+}
+
+func getParams(path string, r *http.Request) (map[string]string, string) {
+
+	routePath := strings.Split(path, "/")
+	requestPath := strings.Split(r.URL.Path, "/")
+
+	params := map[string]string{}
+	for i := len(routePath) - 1; i >= 0; i-- {
+		dir := routePath[i]
+		if dir[0] == ':' {
+			params[dir[1:len(routePath[i])-1]] = requestPath[i]
+			routePath = append(routePath[:i], routePath[i+1:]...)
+		}
+	}
+
+	return params, strings.Join(routePath, "/")
 }
