24c5fed7189da90f3c43ff2f81b5920a14a58aeb
kiricon <kiricon@live.com>
2017-05-06 21:28:36 -0600

    Updated handler tests to be more comprehensive.

--- rapid_test.go
+++ rapid_test.go
@@ -1,6 +1,7 @@
 package rapid
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -8,18 +9,22 @@ import (
 
 func TestServer(t *testing.T) {
 	Get("/", func(c Connection) {
+		fmt.Println("GET:/")
 		c.View("./demo/index.html")
 	})
 
 	Put("/put", func(c Connection) {
+		fmt.Println("PUT:/put")
 		c.Send("Hello")
 	})
 
 	Post("/post", func(c Connection) {
+		fmt.Println("POST:/post")
 		c.Send("Hello")
 	})
 
-	Delete("/Delete", func(c Connection) {
+	Delete("/delete", func(c Connection) {
+		fmt.Println("DELETE:/Delete")
 		c.Send("Hello")
 	})
 
@@ -39,27 +44,37 @@ func TestServer(t *testing.T) {
 
 	server := Listen(3000)
 
-	_, gerr := http.NewRequest("GET", "/", nil)
+	_, gerr := http.Get("http://localhost:3000/")
 	if gerr != nil {
 		t.Fatal(gerr)
 	}
 
-	_, puterr := http.NewRequest("PUT", "/put", nil)
+	_, template := http.Get("http://localhost:3000/hello/Dominic/Valenciana")
+	if template != nil {
+		t.Fatal(template)
+	}
+	_, redirect := http.Get("http://localhost:3000/blah")
+	if redirect != nil {
+		t.Fatal(redirect)
+	}
+	http.Get("http://localhost:3000/")
+
+	_, puterr := http.NewRequest("PUT", "http://localhost:3000/put", nil)
 	if puterr != nil {
 		t.Fatal(puterr)
 	}
 
-	_, posterr := http.NewRequest("POST", "/post", nil)
+	_, posterr := http.Post("http://localhost:3000/post", "", nil)
 	if posterr != nil {
 		t.Fatal(posterr)
 	}
 
-	_, deleteerr := http.NewRequest("DELETE", "/Delete", nil)
+	_, deleteerr := http.NewRequest("DELETE", "http://localhost:3000/delete", nil)
 	if deleteerr != nil {
 		t.Fatal(deleteerr)
 	}
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(3 * time.Second)
 
 	if err := server.Shutdown(nil); err != nil {
 		panic(err) // failure/timeout shutting down the server gracefully
