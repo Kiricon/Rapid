6da99798cb408daeafc83056e4402458a9a4e030
kiricon <kiricon@live.com>
2017-05-07 21:07:01 -0600

    Fixed unit test after refactor

--- paths_test.go
+++ paths_test.go
@@ -7,20 +7,20 @@ import (
 )
 
 func TestAddPath(t *testing.T) {
-	AddPath("/Hello/World", func(c Connection) {})
-	if len(paths) != 1 {
+	AddPath("/Hello/World", func(c Connection) {}, "GET")
+	if len(Paths) != 1 {
 		t.Error("Path not added")
 	}
-	AddPath("/Hello/:World", func(c Connection) {})
-	AddPath("/Foo/:Bar/", func(c Connection) {})
+	AddPath("/Hello/:World", func(c Connection) {}, "GET")
+	AddPath("/Foo/:Bar/", func(c Connection) {}, "GET")
 
-	AddPath("/Foo/:Bar/Baz/:test", func(c Connection) {})
-	AddPath("/Foo/:Bar/Hello/:test", func(c Connection) {})
+	AddPath("/Foo/:Bar/Baz/:test", func(c Connection) {}, "GET")
+	AddPath("/Foo/:Bar/Hello/:test", func(c Connection) {}, "GET")
 
-	fmt.Println("/Hello/World :" + findCorrectPath("/Hello/World"))
-	fmt.Println("/Hello/Foo :" + findCorrectPath("/Hello/Foo"))
-	fmt.Println("/Foo/Baz :" + findCorrectPath("/Foo/Baz"))
-	fmt.Println("/Foo/one/Baz/two :" + findCorrectPath("/Foo/one/Baz/two"))
-	fmt.Println("/Foo/one/Hello/two :" + findCorrectPath("/Foo/one/Hello/two"))
-	fmt.Println("/America : " + findCorrectPath("/America"))
+	fmt.Println("/Hello/World :" + FindCorrectPath("/Hello/World", "GET"))
+	fmt.Println("/Hello/Foo :" + FindCorrectPath("/Hello/Foo", "GET"))
+	fmt.Println("/Foo/Baz :" + FindCorrectPath("/Foo/Baz", "GET"))
+	fmt.Println("/Foo/one/Baz/two :" + FindCorrectPath("/Foo/one/Baz/two", "GET"))
+	fmt.Println("/Foo/one/Hello/two :" + FindCorrectPath("/Foo/one/Hello/two", "GET"))
+	fmt.Println("/America : " + FindCorrectPath("/America", "GET"))
 }
