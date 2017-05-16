e3d6fae968312e44e02ec279b8242a363bb45fe6
kiricon <kiricon@live.com>
2017-05-15 21:48:20 -0600

    Partial file paths are now relative to parent file.

--- templating/file.go
+++ templating/file.go
@@ -9,10 +9,17 @@ import (
 )
 
 func AddPartial(file string) string {
-	return insertPartial(FileString(file))
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return insertPartial(FileString(cwd + "/" + file))
 }
 
-func FileString(file string) string {
+func FileString(file string) (string, string) {
+
+	i := strings.LastIndex(file, "/")
+	dir := file[0:i]
 	filerc, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
@@ -21,15 +28,16 @@ func FileString(file string) string {
 
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(filerc)
-	return buf.String()
+	return buf.String(), dir
 }
 
-func insertPartial(fileString string) string {
+func insertPartial(fileString string, dir string) string {
 	r := regexp.MustCompile("{{\\s*[pP]artial\\s+(.*)\\s*}}")
 	regArr := r.FindAllStringSubmatch(fileString, -1)
 
 	for i := 0; i < len(regArr); i++ {
-		partial := FileString(strings.TrimSpace(regArr[i][1]))
+		path := strings.TrimSpace(dir + "/" + regArr[i][1])
+		partial, _ := FileString(path)
 		fileString = strings.Replace(fileString, regArr[i][0], partial, 1)
 	}
 
